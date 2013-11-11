b396e029fbb78167335a913733e802db5bb562fa
Brady Catherman <brady@orchestrate.io>
2013-11-11 16:55:42 -0700

    Added saucy build targets.

--- package.sh
+++ package.sh
@@ -15,7 +15,8 @@ RELEASE="$(echo "$FULLVERSION" | cut -d- -f2)"
 # Check to see if the given release already exists in the PPA. If so we can not
 # upload so we abort and tell the user to run dch.
 URL="http://ppa.launchpad.net/${PPA}/ubuntu/pool/main/f/flumetail/flumetail_${FULLVERSION}.dsc"
-if curl -s "${URL}" | egrep -q "^Version: ${FULLVERSION}\$" ; then
+GZURL="http://ppa.launchpad.net/${PPA}/ubuntu/pool/main/f/flumetail/flumetail_${VERSION}.orig.tar.gz"
+if curl -fs "${URL}" | egrep -q "^Version: ${FULLVERSION}\$" ; then
   echo "This version already exists in the PPA. Please run dch to update the"
   echo "change log before running this script."
   exit 1
@@ -25,9 +26,14 @@ TEMPDIR="$(mktemp -d /tmp/flumetail.XXXXX)"
 mkdir -p "${TEMPDIR}/flumetail-${VERSION}"
 cp -rf * "${TEMPDIR}/flumetail-${VERSION}/"
 cd "${TEMPDIR}"
-tar -c "flumetail-${VERSION}" | gzip > "flumetail_${VERSION}.orig.tar.gz"
+
+# See if the orig file already exists. If so just use that.
+if ! curl -fs "$GZURL" > "flumetail_${VERSION}.orig.tar.gz" ; then
+  tar -c "flumetail-${VERSION}" | gzip > "flumetail_${VERSION}.orig.tar.gz"
+fi
 
 (cd "${TEMPDIR}/flumetail-${VERSION}/" && debuild -S -sa)
 dput ppa:liquidgecka/flume "flumetail_${FULLVERSION}_source.changes"
 backportpackage -d quantal --upload "ppa:${PPA}" "flumetail_${FULLVERSION}.dsc"
 backportpackage -d precise --upload "ppa:${PPA}" "flumetail_${FULLVERSION}.dsc"
+backportpackage -d raring --upload "ppa:${PPA}" "flumetail_${FULLVERSION}.dsc"
